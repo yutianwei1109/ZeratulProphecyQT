2dc17c7b5e5763594b0f9236ad706ffa5fb0fa24
YuTianwei <837809246@163.com>
2025-11-15 19:44:58 +0800

    MainWindow.m_modelsManager

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,9 @@
+#include <QDebug>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#define _TEST
+// #define _TEST
 #ifdef _TEST
 #include "../test/programtest.h"
 #endif
@@ -9,8 +11,19 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , m_modelsManager(nullptr)
 {
     ui->setupUi(this);
+
+    m_modelsManager = UserListModelsManager::getInstance();
+    qDebug() << "modelsManager address: " << (void*)m_modelsManager;
+
+    ui->listViewFeature->setModel(m_modelsManager->getUserListModel(UserListType::FEATURE));
+    ui->listViewNoFeature->setModel(m_modelsManager->getUserListModel(UserListType::NOFEATURE));
+    ui->listViewExcept->setModel(m_modelsManager->getUserListModel(UserListType::EXCEPT));
+    ui->listViewPossible->setModel(m_modelsManager->getUserListModel(UserListType::POSSIBLE));
+    
+    m_modelsManager->reset();
 
 #ifdef _TEST
     programTest test;
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include "src/model/userlistmodelsmanager.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,5 +19,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    UserListModelsManager *m_modelsManager;
 };
 #endif // MAINWINDOW_H
